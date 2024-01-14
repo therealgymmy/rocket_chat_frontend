c6f2927fa71c5d269b9e502863e9a3c30a3600ca
Jimmy Lu <jimmy.lu.2011@gmail.com>
2024-01-14 12:46:37 -0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 export default function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // Load existing messages from the server
+    // fetch('http://<your-server-ip>:8000/messages')
+    fetch('http://127.0.0.1:8000/messages')
+      .then(response => response.json())
+      .then(data => {
+        setMessages(data.map(m => ({
+          _id: Math.random().toString(36).substring(7),
+          text: m.content,
+          createdAt: new Date(),
+          user: {
+            _id: m.username,
+            name: m.username,
+          },
+        })));
+      });
+  }, []);
+
+  const onSend = (newMessages = []) => {
+    console.log(newMessages);
+    setMessages(GiftedChat.append(messages, newMessages));
+    // Send the message to the server
+    fetch('http://127.0.0.1:8000/message', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: newMessages[0].user._id,
+        content: newMessages[0].text,
+      }),
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 'user1', // The ID of the local user
+      }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
